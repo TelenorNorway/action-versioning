a4509d139a2bb5e4a0213977188c71e82599c530
Jameson <james.bradlee@telenor.no>
2023-07-19 15:31:58 +0200

    v=0.1.2

--- src/strategies/semver.ts
+++ src/strategies/semver.ts
@@ -61,6 +61,6 @@ async function getTags(token: string, repository: string): Promise<string[]> {
 	debug(out);
 	return out
 		.split(/[\r\n]+/g)
-		.map((line) => line.substring(59))
+		.map((line) => line.substring(41))
 		.filter((tag) => REGEX.test(tag));
 }
